ad2a110671e9e6a9c3b0b45a14c80532e0f28a94
Rajat Pal <rajatpalbnk@gmail.com>
2024-04-20 00:49:15 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -12,6 +13,8 @@ import {app, server} from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming request with json playloads from req.body
@@ -21,6 +24,12 @@ app.use(cookieParser()); // to parse the incoming request with json playloads fr
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World!!!');
